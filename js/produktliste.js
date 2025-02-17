096ca92317e65dd487433321fcb02741b1d89591
Henrik von Oettingen <heoe0001@stud.kea.dk>
2025-02-17 13:14:21 +0100

    henter-produkter-dynamisk-henrik

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,15 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
-const season = urlParams.get("season");
+const category = urlParams.get("category");
 
-if (season) {
-  document.getElementById("category-title").textContent = season;
-  fetchProducts(season);
+if (category) {
+  document.querySelector(".category-title").textContent = category;
+  fetchProducts(category);
 }
 
-async function fetchProducts(season) {
-  const url = `https://kea-alt-del.dk/t7/api/products?season=${season}`;
+async function fetchProducts(category) {
+  const url = `https://dummyjson.com/products`;
   const response = await fetch(url);
-  const products = await response.json();
+  const data = await response.json();
+  const products = data.products.filter((product) => product.category.toLowerCase() === category.toLowerCase());
 
   displayProducts(products);
 }
@@ -19,22 +20,24 @@ function displayProducts(products) {
   container.innerHTML = ""; // Ryd tidligere indhold
 
   products.forEach((product) => {
-    const discountLabel = product.discount ? `<div class="discount-label">Udsalg: ${product.discount}%</div>` : "";
+    const discountLabel = product.discountPercentage ? `<div class="discount-label">Udsalg: ${product.discountPercentage}%</div>` : "";
 
-    const soldoutLabel = product.soldout ? `<div class="soldout-label">Udsolgt</div>` : "";
+    const soldoutLabel = product.stock === 0 ? `<div class="soldout-label">Udsolgt</div>` : "";
 
     const productHTML = `
-      <section class="card">
-      <a href="produkt.html?id=${product.id}">
-         ${discountLabel}
-         ${soldoutLabel}
-          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
-          <article class="card_container">
-          <h2>${product.productdisplayname}</h2>
-          <p class="price">${product.price} DKK</p>
-          </article>
-      </a>
-      </section>
+        <section class="card">
+          <a href="produkt.html?id=${product.id}">
+            ${soldoutLabel}
+            <img src="${product.thumbnail}" alt="${product.title}" />
+            <article class="card_container">
+              <h2 class="item_titel">${product.title}</h2>
+              <div class="arrow">
+                <p class="price">${product.price} $</p>
+                <img src="img/right-arrow.webp" alt="arrow" style="width: 30px; height: 40px" />
+              </div>
+            </article>
+          </a>
+        </section>
     `;
     container.innerHTML += productHTML;
   });
